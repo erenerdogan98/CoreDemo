72195953f413bd0b976e69ee5a560a1013fda9d0
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-20 21:45:41 +0300

    added Category viewcomponent

---
+++ CoreDemo/ViewComponents/Category/CategoryList.cs
@@ -0,0 +1,19 @@
+﻿using BLL.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Category
+{
+	public class CategoryList : ViewComponent
+	{
+		private readonly ICategoryService _categoryService;
+        public CategoryList(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var values = _categoryService.GetAllAsync();
+            return View(values);
+        }
+    }
+}
