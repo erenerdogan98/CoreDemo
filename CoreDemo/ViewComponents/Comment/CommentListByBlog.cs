5dbfdf7ef025400c652141ed28d8a3e1ecd2b8f2
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-20 21:19:24 +0300

    added commetlistbyblog class

--- CoreDemo/ViewComponents/Comment/CommentListByBlog.cs
+++ CoreDemo/ViewComponents/Comment/CommentListByBlog.cs
@@ -1,8 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BLL.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.ViewComponents.Comment
 {
 	public class CommentListByBlog : ViewComponent
 	{
+		private readonly ICommentService _commentService;
+        public CommentListByBlog(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+        public IViewComponentResult Invoke()
+		{
+			var values = _commentService.GetByIdAsync(2);
+			return View();
+		}
 	}
 }
