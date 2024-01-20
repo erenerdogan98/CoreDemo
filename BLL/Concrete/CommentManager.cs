b1eda639a44cf8be8bb973704dcd2a867de60fa2
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-20 20:57:12 +0300

    added category manager and implemented methods ..

---
+++ BLL/Concrete/CommentManager.cs
@@ -0,0 +1,32 @@
+﻿using BLL.Abstract;
+using DAL.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Concrete
+{
+	internal class CommentManager : ICommentService
+	{
+		private readonly ICommentDAL _commentDAL;
+        public CommentManager(ICommentDAL commentDAL)
+        {
+            _commentDAL = commentDAL ?? throw new ArgumentNullException(nameof(_commentDAL));
+        }
+        public async Task AddAsync(Comment comment) => await _commentDAL.AddAsync(comment);
+
+		public async Task DeleteAsync(Comment comment) => await _commentDAL.DeleteAsync(comment);
+
+		public async Task<IEnumerable<Comment>> GetAllAsync() => await _commentDAL.GetAllAsync();
+
+		public async Task<IEnumerable<Comment>> GetAllAsync(Expression<Func<Comment, bool>> filter) => await _commentDAL.GetAllAsync(filter);
+
+		public async Task<Comment> GetByIdAsync(int id) => await _commentDAL.GetByIdAsync(id);
+
+		public async Task UpdateAsync(Comment comment) => await _commentDAL.UpdateAsync(comment);
+	}
+}
