6bf799d885066fb5a6cb68fa02d71ad033e15968
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-20 15:12:09 +0300

    generic repository added

---
+++ DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using DAL.Abstract;
+using DAL.Context;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace DAL.Repositories
+{
+    public class GenericRepository<T> : IGenericDAL<T> where T : class , IEntityBase, new()
+    {
+        private readonly MyContext _context;
+        public GenericRepository(MyContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(_context));
+        }
+        private async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+        private IQueryable<T> QueryWithIncludes(params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = _context.Set<T>();
+            return includeProperties.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+        }
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _context.Remove(entity);
+            await SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync() => await QueryWithIncludes().ToListAsync();
+
+        public async Task<T> GetByIdAsync(int id) => await QueryWithIncludes().FirstOrDefaultAsync(x => x.ID == id);
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
