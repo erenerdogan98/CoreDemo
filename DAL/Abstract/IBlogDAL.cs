1c9ba81fa7b59147ed717e5055bd1c79d1653a9d
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-20 18:36:17 +0300

    speacial method for getting blogs with category

--- DAL/Abstract/IBlogDAL.cs
+++ DAL/Abstract/IBlogDAL.cs
@@ -4,5 +4,6 @@ namespace DAL.Abstract
 {
     public interface IBlogDAL : IGenericDAL<Blog>
     {
+        Task<List<Blog>> GetListWithCategoryAsync();
     }
 }
--- DAL/EntityFramework/EFBlogRepository.cs
+++ DAL/EntityFramework/EFBlogRepository.cs
@@ -2,14 +2,24 @@
 using DAL.Context;
 using DAL.Repositories;
 using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace DAL.EntityFramework
 {
     public class EFBlogRepository : GenericRepository<Blog>, IBlogDAL
     {
+        private readonly MyContext _context;
         public EFBlogRepository(MyContext context) : base(context)
         {
-
+            _context = context;
         }
+        private IQueryable<Blog> CustomQueryWithIncludes(params Expression<Func<Blog, object>>[] includeProperties)
+        {
+            IQueryable<Blog> query = _context.Set<Blog>();
+            return includeProperties.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+        }
+
+        public async Task<List<Blog>> GetListWithCategoryAsync() => await CustomQueryWithIncludes(x => x.Category).ToListAsync();
     }
 }
