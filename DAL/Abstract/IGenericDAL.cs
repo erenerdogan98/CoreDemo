b5cbd871adc41819cd5caf14b3ac99f2b23eb30e
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-20 14:53:19 +0300

    added generic interface (async)

---
+++ DAL/Abstract/IGenericDAL.cs
@@ -0,0 +1,12 @@
+﻿
+namespace DAL.Abstract
+{
+    public interface IGenericDAL<T> where T : class
+    {
+        Task AddAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+    }
+}
