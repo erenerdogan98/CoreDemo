71ff6181572343cf851e2fe95bfe15e0e12b1023
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-20 17:57:24 +0300

    added blog controller

--- CoreDemo/Controllers/BlogController.cs
+++ CoreDemo/Controllers/BlogController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BLL.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
     public class BlogController : Controller
     {
+        private readonly IBlogService _blogService;
+        public BlogController(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
         public IActionResult Index()
         {
-            return View();
+            var values = _blogService.GetAllAsync();
+            return View(values);
         }
     }
 }
