995bc404472ab204ebf7fc00119de01c57a92204
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-20 14:16:10 +0300

    added context for Sql connection

---
+++ DAL/Context/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace DAL.Context
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-57R498V\\SQLEXPRESS01;database=CoreBlogDb; integrated security=true");
+        }
+    }
+}
