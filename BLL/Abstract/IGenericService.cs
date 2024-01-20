25131fa1f927b80544c60f73cf789fff6ab5951b
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-20 15:30:53 +0300

    Generic Service added

---
+++ BLL/Abstract/IGenericService.cs
@@ -0,0 +1,11 @@
+﻿namespace BLL.Abstract
+{
+    public interface IGenericService<T> where T : class
+    {
+        Task AddAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+    }
+}
