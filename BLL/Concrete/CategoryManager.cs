eea711a3b8ace2f6d816574db8d694f66d1ed04c
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-20 15:40:18 +0300

    category manager added

---
+++ BLL/Concrete/CategoryManager.cs
@@ -0,0 +1,21 @@
+﻿using BLL.Abstract;
+using DAL.Abstract;
+using Entities.Concrete;
+
+namespace BLL.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        private readonly ICategoryDAL _categoryDAL;
+        public async Task AddAsync(Category category) => await _categoryDAL.AddAsync(category);
+
+
+        public async Task DeleteAsync(Category category) => await _categoryDAL.DeleteAsync(category);
+
+        public async Task<IEnumerable<Category>> GetAllAsync() => await _categoryDAL.GetAllAsync();
+
+        public async Task<Category> GetByIdAsync(int id) => await _categoryDAL.GetByIdAsync(id);
+
+        public async Task UpdateAsync(Category category) => await _categoryDAL.UpdateAsync(category);
+    }
+}
