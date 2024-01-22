3cccc4ac53bc3c0bce8b8dc6b623260b5ac345d5
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-22 14:31:48 +0300

    added GetBlogsByWriter ..

--- DAL/Abstract/IBlogDAL.cs
+++ DAL/Abstract/IBlogDAL.cs
@@ -5,5 +5,6 @@ namespace DAL.Abstract
     public interface IBlogDAL : IGenericDAL<Blog>
     {
         Task<List<Blog>> GetListWithCategoryAsync();
+        Task<List<Blog>> GetBlogsByWriter(int id);
     }
 }
