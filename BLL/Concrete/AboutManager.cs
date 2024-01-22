7a3f945069ea74431443d9adbe4854e268f07150
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-22 17:43:26 +0300

    added About Manager ..

---
+++ BLL/Concrete/AboutManager.cs
@@ -0,0 +1,27 @@
+﻿using BLL.Abstract;
+using DAL.Abstract;
+using Entities.Concrete;
+using System.Linq.Expressions;
+
+namespace BLL.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        private readonly IAboutDAL _aboutDAL;
+        public AboutManager(IAboutDAL aboutDAL)
+        {
+            _aboutDAL = aboutDAL ?? throw new ArgumentNullException(nameof(_aboutDAL));
+        }
+        public async Task AddAsync(About about) => await _aboutDAL.AddAsync(about);
+
+        public async Task DeleteAsync(About about) => await _aboutDAL.DeleteAsync(about);
+
+        public async Task<IEnumerable<About>> GetAllAsync() => await _aboutDAL.GetAllAsync();
+
+        public async Task<IEnumerable<About>> GetAllAsync(Expression<Func<About, bool>> filter) => await _aboutDAL.GetAllAsync(filter);
+
+        public async Task<About> GetByIdAsync(int id) => await _aboutDAL.GetByIdAsync(id);
+
+        public async Task UpdateAsync(About about) => await _aboutDAL.UpdateAsync(about);
+    }
+}
