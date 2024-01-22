d97b7ec87aac169229fb8074103f5d98dfb07fb2
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-22 14:09:35 +0300

    added rules

--- BLL/ValidationRules/WriterValidator.cs
+++ BLL/ValidationRules/WriterValidator.cs
@@ -7,7 +7,13 @@ namespace BLL.ValidationRules
 	{
         public WriterValidator()
         {
-            
-        }
-    }
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Writer name can't be null or empty!");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Mail address can't be null or empty!");
+			RuleFor(x => x.Password)
+			.NotEmpty().WithMessage("Password can't be null or empty!")
+			.MinimumLength(8).WithMessage("Password must consist of at least 8 characters")
+			.Matches(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$")
+				.WithMessage("The password must contain at least 1 uppercase letter, 1 lowercase letter and 1 number.");
+		}
+	}
 }
