8975f25ffe35ef61c3b244e4e8673637d4329095
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-22 16:08:27 +0300

    added newsletter controller ..

---
+++ CoreDemo/Controllers/NewsLetterController.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class NewsLetterController : Controller
+    {
+        [HttpGet]
+        public PartialViewResult SubscribeMail()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult SubscribeMail(NewsLetter newsLetter)
+        {
+            return PartialView();
+        }
+    }
+}
