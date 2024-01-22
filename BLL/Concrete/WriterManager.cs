4705e823b30e56a5f0c1f742d01d32c78f2b831c
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-22 12:52:36 +0300

    implemented IWriterService ..

--- BLL/Concrete/WriterManager.cs
+++ BLL/Concrete/WriterManager.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using BLL.Abstract;
+using DAL.Abstract;
+using Entities.Concrete;
+using System.Linq.Expressions;
 
 namespace BLL.Concrete
 {
-	internal class WriterManager
+	public class WriterManager : IWriterService
 	{
+		private readonly IWriterDAL _writerDAL;
+        public WriterManager(IWriterDAL writerDAL)
+        {
+            _writerDAL = writerDAL;
+        }
+        public async Task AddAsync(Writer writer) => await _writerDAL.AddAsync(writer);
+
+		public async Task DeleteAsync(Writer writer) => await _writerDAL.DeleteAsync(writer);
+
+		public async Task<IEnumerable<Writer>> GetAllAsync() => await _writerDAL.GetAllAsync();
+
+		public async Task<IEnumerable<Writer>> GetAllAsync(Expression<Func<Writer, bool>> filter) => await _writerDAL.GetAllAsync(filter);
+
+		public async Task<Writer> GetByIdAsync(int id) => await _writerDAL.GetByIdAsync(id);
+
+		public async Task UpdateAsync(Writer writer) => await _writerDAL.UpdateAsync(writer);
 	}
 }
