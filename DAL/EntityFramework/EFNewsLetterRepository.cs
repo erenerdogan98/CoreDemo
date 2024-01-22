276848901c4b4372ea060d8531345acafd801713
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-22 16:03:51 +0300

    added for entity framewrok repository ..

---
+++ DAL/EntityFramework/EFNewsLetterRepository.cs
@@ -0,0 +1,15 @@
+﻿using DAL.Abstract;
+using DAL.Context;
+using DAL.Repositories;
+using Entities.Concrete;
+
+namespace DAL.EntityFramework
+{
+    public class EFNewsLetterRepository : GenericRepository<NewsLetter>, INewsLetterDAL
+    {
+        public EFNewsLetterRepository(MyContext context) : base(context)
+        {
+
+        }
+    }
+}
