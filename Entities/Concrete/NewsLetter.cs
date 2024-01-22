5e8e80c4f2077421bd5bca84cacb3e5714598067
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-22 15:54:48 +0300

    added new enttiy ..

---
+++ Entities/Concrete/NewsLetter.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Concrete
+{
+    public class NewsLetter : IEntityBase
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Mail { get; set; }
+        public string MailStatus { get; set; }
+    }
+}
