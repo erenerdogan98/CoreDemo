e0b4d299b326066e9eb828e3f56e146372edb389
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-22 14:50:30 +0300

    added viev component

---
+++ CoreDemo/ViewComponents/Blog/WriterLastBlog.cs
@@ -0,0 +1,19 @@
+﻿using BLL.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Blog
+{
+	public class WriterLastBlog : ViewComponent
+	{
+		private readonly IBlogService _blogService;
+        public WriterLastBlog(IBlogService blogService)
+        {
+            _blogService = blogService ?? throw new ArgumentNullException(nameof(_blogService));
+        }
+        public IViewComponentResult Invoke()
+        {
+            var values = _blogService.GetBlogsByWriterAsync(1);
+            return View(values);
+        }
+    }
+}
