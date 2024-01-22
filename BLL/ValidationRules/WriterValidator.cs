b240b62ad18e037b6ee52af608dcd6bbdca0881f
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-22 14:04:58 +0300

    added writer validator

---
+++ BLL/ValidationRules/WriterValidator.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+
+namespace BLL.ValidationRules
+{
+	public class WriterValidator : AbstractValidator<Writer>
+	{
+        public WriterValidator()
+        {
+            
+        }
+    }
+}
