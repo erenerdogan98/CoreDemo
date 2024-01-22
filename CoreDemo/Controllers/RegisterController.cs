6d91f17d8b3ad439f717001907a627cc16a0c23b
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-22 12:56:24 +0300

    added get and post methods

--- CoreDemo/Controllers/RegisterController.cs
+++ CoreDemo/Controllers/RegisterController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BLL.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
 	public class RegisterController : Controller
 	{
+		private readonly IWriterService _writerService;
+        public RegisterController(IWriterService writerService)
+        {
+            _writerService = writerService;
+        }
+        [HttpGet]
 		public IActionResult Index()
 		{
 			return View();
+		}
+		[HttpPost]
+		public IActionResult Index(Writer writer)
+		{
+			writer.Status = true;
+			writer.About = "Test";
+			_writerService.AddAsync(writer);
+			return RedirectToAction("Index", "Blog");
 		}
 	}
 }
