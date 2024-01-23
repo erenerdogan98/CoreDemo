f95e438d7e872fc29f1eb292fab6daaea3bb8778
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-23 22:16:48 +0300

    added ..

--- CoreDemo/ViewComponents/Blog/BlogListDashboard.cs
+++ CoreDemo/ViewComponents/Blog/BlogListDashboard.cs
@@ -1,8 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BLL.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.ViewComponents.Blog
 {
     public class BlogListDashboard : ViewComponent
     {
+        private readonly IBlogService _blogService;
+        public BlogListDashboard(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var values = _blogService.GetListWithCategoryAsync();
+            return View(values);
+        }
     }
 }
