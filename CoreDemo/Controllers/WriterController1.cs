1cbc7c0ebbba257ad70a2412740e831f910c9c26
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-23 15:55:27 +0300

    added writer controller

---
+++ CoreDemo/Controllers/WriterController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+	public class WriterController1 : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
