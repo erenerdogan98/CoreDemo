5231c1b5b04f10f7bbadf1bb73f69a7e3499585a
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-23 20:58:22 +0300

    added Task<List<Blog>> GetListWithCategoryAsync(int id);

--- BLL/Abstract/IBlogService.cs
+++ BLL/Abstract/IBlogService.cs
@@ -6,6 +6,6 @@ namespace BLL.Abstract
 	{
         Task<List<Blog>> GetListWithCategoryAsync();
 		Task<List<Blog>> GetBlogsByWriterAsync(int id);
-
-	}
+        Task<List<Blog>> GetListWithCategoryAsync(int id);
+    }
 }
