bd8fba860b737b22fdafacceec36e74bb78a809b
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-23 15:57:31 +0300

    added Authorize

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
 	public class WriterController : Controller
 	{
+		[Authorize]
 		public IActionResult Index()
 		{
 			return View();
