26cb4e9e5771bf89c60f2d1e10bc67b4e26c6211
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-23 14:48:57 +0300

    added method for test ..

--- CoreDemo/Controllers/ContactController.cs
+++ CoreDemo/Controllers/ContactController.cs
@@ -1,18 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BLL.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
     public class ContactController : Controller
     {
+        private readonly IContactService _contactService;
+        public ContactController(IContactService contactService)
+        {
+            _contactService = contactService ?? throw new ArgumentNullException(nameof(_contactService));
+        }
         [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Index()
+        public IActionResult Index(Contact contact)
         {
-            return View();
+            contact.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            contact.Status = true;
+            _contactService.AddAsync(contact);
+            return RedirectToAction("Index", "Blog");
         }
     }
 }
