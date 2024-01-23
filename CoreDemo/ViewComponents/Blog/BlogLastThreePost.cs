bfed98b58a8a286a0e4568ed5cfc7e4bfe76b8b8
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-23 16:50:33 +0300

    added view component ..

---
+++ CoreDemo/ViewComponents/Blog/BlogLastThreePost.cs
@@ -0,0 +1,19 @@
+﻿using BLL.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Blog
+{
+    public class BlogLastThreePost : ViewComponent
+    {
+        private readonly IBlogService _blogServce;
+        public BlogLastThreePost(IBlogService blogService)
+        {
+            _blogServce = blogService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            // will write later
+            return View();
+        }
+    }
+}
