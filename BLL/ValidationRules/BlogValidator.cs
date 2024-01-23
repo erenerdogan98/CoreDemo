fa6894423a5f28cc0e9caee3d04788a672d71994
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-23 20:05:15 +0300

    added validation rules

--- BLL/ValidationRules/BlogValidator.cs
+++ BLL/ValidationRules/BlogValidator.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Entities.Concrete;
+using FluentValidation;
 
 namespace BLL.ValidationRules
 {
-    internal class BlogValidator
+    public class BlogValidator : AbstractValidator<Blog>
     {
+        public BlogValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Blog title can not be null or empty.");
+            RuleFor(x => x.Content).NotEmpty().WithMessage("Blog content can not be null or empty.");
+            RuleFor(x => x.Image).NotEmpty().WithMessage("Blog image can not be null or empty.");
+
+            RuleFor(x => x.Title).MaximumLength(159).WithMessage("Please , write your title less than 160 character!");
+            RuleFor(x => x.Title).MinimumLength(5).WithMessage("Please , write your title more than 4 character!");
+        }
     }
 }
