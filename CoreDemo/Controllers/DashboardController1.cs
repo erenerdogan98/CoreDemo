6a8f124c81ba6698f9a46713a3aebd5becafa18c
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-23 21:38:17 +0300

    created dashboard controller

---
+++ CoreDemo/Controllers/DashboardController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class DashboardController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
