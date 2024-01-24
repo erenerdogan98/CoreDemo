9f51791bc4864b51d4bed1f04f383d573982a123
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-24 14:46:18 +0300

    added new entity for Blog ratings ..

---
+++ Entities/Concrete/BlogRating.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities.Concrete
+{
+    public class BlogRating : IEntityBase
+    {
+        public int ID { get; set; }
+        public int BlogID { get; set; }
+        public int BlogTotalScore { get; set; }
+        public int BlogRatingCount { get; set; }
+    }
+}
