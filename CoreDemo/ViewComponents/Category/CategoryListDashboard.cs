3de46fe45e8dd2a0f12c7567da861463ba807b59
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-24 12:37:47 +0300

    added CategoryList for dashboard

---
+++ CoreDemo/ViewComponents/Category/CategoryListDashboard.cs
@@ -0,0 +1,19 @@
+﻿using BLL.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Category
+{
+    public class CategoryListDashboard : ViewComponent
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryListDashboard(ICategoryService categoryService)
+        {
+            _categoryService = categoryService ?? throw new ArgumentNullException(nameof(_categoryService));
+        }
+        public IViewComponentResult Invoke()
+        {
+            var values = _categoryService.GetAllAsync();
+            return View(values);
+        }
+    }
+}
