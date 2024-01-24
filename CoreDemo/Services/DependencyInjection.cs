72221af6952ed35069c66fe3da84ce4fa02a6241
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-24 17:38:46 +0300

    added some dependencies ..

---
+++ CoreDemo/Services/DependencyInjection.cs
@@ -0,0 +1,37 @@
+﻿using BLL.Abstract;
+using BLL.Concrete;
+using DAL.Abstract;
+using DAL.Context;
+using DAL.EntityFramework;
+
+namespace CoreDemo.Services
+{
+    public static class DependencyInjection
+    {
+        public static void ConfigureMyServices(this IServiceCollection services)
+        {
+            services.AddDbContext<MyContext>();
+
+            services.AddScoped<IAboutService, AboutManager>();
+            services.AddScoped<IAboutDAL, EFAboutRepository>();
+
+            services.AddScoped<IBlogService, BlogManager>();
+            services.AddScoped<IBlogDAL , EFBlogRepository>();
+
+            services.AddScoped<ICategoryService, CategoryManager>();
+            services.AddScoped<ICategoryDAL, EFCategoryRepository>();
+
+            services.AddScoped<ICommentService, CommentManager>();
+            services.AddScoped<ICommentDAL, EFCommentRepository>();
+
+            services.AddScoped<IContactService, ContactManager>();
+            services.AddScoped<IContactDAL, EFContactRepository>();
+
+            services.AddScoped<INewsLetterService, NewsLetterManager>();
+            services.AddScoped<INewsLetterDAL, EFNewsLetterRepository>();
+
+            services.AddScoped<IWriterService, WriterManager>();
+            services.AddScoped<IWriterDAL, EFWriterRepository>();
+        }
+    }
+}
