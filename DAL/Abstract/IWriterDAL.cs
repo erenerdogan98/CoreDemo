1dca7bf16b834f1673828a3e822c342a9b9daf3f
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-24 15:30:44 +0300

    added GetWriterById method ...

--- DAL/Abstract/IWriterDAL.cs
+++ DAL/Abstract/IWriterDAL.cs
@@ -4,5 +4,6 @@ namespace DAL.Abstract
 {
     public interface IWriterDAL : IGenericDAL<Writer>
     {
+        Task<List<Writer>> GetWriterByIdAsync(int id); // for admin panels , will use 
     }
 }
--- DAL/EntityFramework/EFWriterRepository.cs
+++ DAL/EntityFramework/EFWriterRepository.cs
@@ -2,15 +2,25 @@
 using DAL.Context;
 using DAL.Repositories;
 using Entities.Concrete;
-
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace DAL.EntityFramework
 {
     public class EFWriterRepository : GenericRepository<Writer>, IWriterDAL
     {
+        private readonly MyContext _context;
         public EFWriterRepository(MyContext context) : base(context)
         {
+            _context = context ?? throw new ArgumentNullException(nameof(_context));
+        }
 
+        public async Task<List<Writer>> GetWriterByIdAsync(int id) => await CustomQueryWithIncludes(x => x.ID == id).ToListAsync();
+
+        private IQueryable<Writer> CustomQueryWithIncludes(params Expression<Func<Writer, object>>[] includeProperties)
+        {
+            IQueryable<Writer> query = _context.Set<Writer>();
+            return includeProperties.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
         }
     }
 }
