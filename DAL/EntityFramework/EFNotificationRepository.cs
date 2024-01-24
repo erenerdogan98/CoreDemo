6cf0735de3279a78719fb1b0043e4d8b0ec05504
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-24 18:05:12 +0300

    added EF notification repository

---
+++ DAL/EntityFramework/EFNotificationRepository.cs
@@ -0,0 +1,15 @@
+﻿using DAL.Abstract;
+using DAL.Context;
+using DAL.Repositories;
+using Entities.Concrete;
+
+namespace DAL.EntityFramework
+{
+    public class EFNotificationRepository : GenericRepository<Notification>, INotificationDAL
+    {
+        public EFNotificationRepository(MyContext context) : base(context)
+        {
+            
+        }
+    }
+}
