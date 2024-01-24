883c473d26a0f47c97f99c0b66cad074790516b4
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-24 15:49:12 +0300

    added ViewBags .. i'll refactor controller

--- CoreDemo/Controllers/DashboardController.cs
+++ CoreDemo/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DAL.Context;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
@@ -8,6 +9,10 @@ namespace CoreDemo.Controllers
     {
         public IActionResult Index()
         {
+            MyContext c = new MyContext();
+            ViewBag.v1 = c.Blogs.Count().ToString();
+            ViewBag.v2 = c.Blogs.Where(x => x.WriterID == 1).Count().ToString();
+            ViewBag.v3 = c.Categories.Count().ToString();
             return View();
         }
     }
