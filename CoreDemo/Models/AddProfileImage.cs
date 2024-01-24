30c2663cbc57f6a2c302950a30b31bf7b2438bd8
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-24 17:20:08 +0300

    changed Image type , IFormFile

--- CoreDemo/Models/AddProfileImage.cs
+++ CoreDemo/Models/AddProfileImage.cs
@@ -5,7 +5,7 @@
         public int ID { get; set; }
         public string Name { get; set; }
         public string About { get; set; }
-        public string Image { get; set; }
+        public IFormFile Image { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public bool Status { get; set; }
