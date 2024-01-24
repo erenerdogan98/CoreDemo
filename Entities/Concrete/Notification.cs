75469d221a3026016c141f18828c6a6a7d74ec32
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-24 17:46:28 +0300

    added notification entity

---
+++ Entities/Concrete/Notification.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Concrete
+{
+    public class Notification : IEntityBase
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Type { get; set; }
+        public string TypeSymbol { get; set; }
+        public string Details { get; set; }
+        public DateTime Date { get; set; }
+        public bool Status { get; set; }
+    }
+}
