507692dfcd1ef521d05f2b1b0b60c32a472fec5a
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-24 15:52:01 +0300

    added WriterEditProfile method

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -1,37 +1,49 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BLL.Abstract;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
-	public class WriterController : Controller
-	{
-		[Authorize]
-		public IActionResult Index()
-		{
-			return View();
-		}
-		public IActionResult WriterProfile()
-		{
-			return View();
-		}
-		public IActionResult WriterMail() 
-		{
-			return View();
-		}
-		[AllowAnonymous]
-		public IActionResult Test()
-		{
-			return View();
-		}
-		[AllowAnonymous]
-		public PartialViewResult WriterNavbarPartial()
-		{
-			return PartialView();
-		}
-		[AllowAnonymous]
-		public PartialViewResult WriterFooterPartial()
-		{
-			return PartialView();
-		}
-	}
+    public class WriterController : Controller
+    {
+        private readonly IWriterService _writerService;
+        public WriterController(IWriterService writerService)
+        {
+            _writerService = writerService;
+        }
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WriterProfile()
+        {
+            return View();
+        }
+        public IActionResult WriterMail()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public IActionResult Test()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public PartialViewResult WriterNavbarPartial()
+        {
+            return PartialView();
+        }
+        [AllowAnonymous]
+        public PartialViewResult WriterFooterPartial()
+        {
+            return PartialView();
+        }
+        [AllowAnonymous]
+        public IActionResult WriterEditProfile()
+        {
+            var writerValues = _writerService.GetByIdAsync(1);
+            return View(writerValues);
+        }
+    }
 }
