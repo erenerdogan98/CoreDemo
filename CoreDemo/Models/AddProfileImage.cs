e55abcb9b20cc624813a005d8ee8d23ef295c9d6
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-24 17:19:29 +0300

    added , props from Writer (entity)

---
+++ CoreDemo/Models/AddProfileImage.cs
@@ -0,0 +1,13 @@
+﻿namespace CoreDemo.Models
+{
+    public class AddProfileImage
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string About { get; set; }
+        public string Image { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool Status { get; set; }
+    }
+}
