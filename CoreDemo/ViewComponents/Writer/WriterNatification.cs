841251244201ec645d275d28a70d42c5961b632a
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-24 18:45:33 +0300

    added list method ..

--- CoreDemo/ViewComponents/Writer/WriterNatification.cs
+++ CoreDemo/ViewComponents/Writer/WriterNatification.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BLL.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.ViewComponents.Writer
 {
     public class WriterNatification : ViewComponent
     {
+        private readonly INotificationService _notificationService;
+        public WriterNatification(INotificationService notificationService)
+        {
+            _notificationService = notificationService ?? throw new ArgumentNullException(nameof(notificationService));
+        }
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = _notificationService.GetAllAsync();
+            return View(values);
         }
     }
 }
