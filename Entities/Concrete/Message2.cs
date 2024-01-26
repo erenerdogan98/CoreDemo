c71c40cf3e52e19f10c323291062fec4e3f8a111
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-26 22:22:29 +0300

    added new table Message2

---
+++ Entities/Concrete/Message2.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Concrete
+{
+    public class Message2 : IEntityBase
+    {
+        [Key]
+        public int ID { get; set; }
+        public int? Sender { get; set; }
+        public int? Receiver { get; set; }
+        public string Subject { get; set; }
+        public string Details { get; set; }
+        public DateTime Date { get; set; }
+        public bool Status { get; set; }
+    }
+}
