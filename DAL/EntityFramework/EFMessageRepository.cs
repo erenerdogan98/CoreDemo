e4c3fc9a9b5c70d95737d777b950006d8c44fdde
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-26 17:31:59 +0300

    implemented GetInBoxListByWriter method and added ..

--- DAL/EntityFramework/EFMessageRepository.cs
+++ DAL/EntityFramework/EFMessageRepository.cs
@@ -1,14 +1,27 @@
-﻿using DAL.Context;
+﻿using DAL.Abstract;
+using DAL.Context;
 using DAL.Repositories;
 using Entities;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace DAL.EntityFramework
 {
-    public class EFMessageRepository : GenericRepository<Message>
+    public class EFMessageRepository : GenericRepository<Message> , IMessageDAL
     {
+        private readonly MyContext _context;
         public EFMessageRepository(MyContext context) : base(context)
         {
-            
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<List<Message>> GetInBoxListByWriter(string receiver) => await CustomQueryWithIncludes(x => x.Receiver == receiver).ToListAsync();
+
+        private IQueryable<Message> CustomQueryWithIncludes(params Expression<Func<Message, object>>[] includeProperties)
+        {
+            IQueryable<Message> query = _context.Set<Message>();
+            return includeProperties.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
         }
     }
 }
