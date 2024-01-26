e3bcf20f2468200cdc5f9e7bb24684a76573495a
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-26 17:22:23 +0300

    added EntityFramework Message repository

---
+++ DAL/EntityFramework/EFMessageRepository.cs
@@ -0,0 +1,14 @@
+﻿using DAL.Context;
+using DAL.Repositories;
+using Entities;
+
+namespace DAL.EntityFramework
+{
+    public class EFMessageRepository : GenericRepository<Message>
+    {
+        public EFMessageRepository(MyContext context) : base(context)
+        {
+            
+        }
+    }
+}
