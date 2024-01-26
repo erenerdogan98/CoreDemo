80c9f3f7e46fb869a1b55e7a8e999c50cd164d5e
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-26 18:15:15 +0300

    called NotificationService and add AllNotification method

--- CoreDemo/Controllers/NotificationController.cs
+++ CoreDemo/Controllers/NotificationController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BLL.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
     public class NotificationController : Controller
     {
+        private readonly INotificationService _notificationService;
+        public NotificationController(INotificationService notificationService)
+        {
+            _notificationService = notificationService ?? throw new ArgumentNullException(nameof(notificationService));
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        public async Task<IActionResult> AllNotification() 
+        {
+            var values = await _notificationService.GetAllAsync();
+            return View(values);
         }
     }
 }
