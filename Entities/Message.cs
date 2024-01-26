c924be746a09d8333e56c749064179d260f6fb9e
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-26 17:18:11 +0300

    added new table Message ..

---
+++ Entities/Message.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class Message : IEntityBase
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public string Subject { get; set; }
+        public string Details { get; set; }
+        public DateTime Date { get; set; }
+        public bool Status { get; set; }
+    }
+}
