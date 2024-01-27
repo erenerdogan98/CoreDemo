4f66dd8c4f211eb47d46989a6c6669a8fc952f4c
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-27 15:18:43 +0300

    added Message Controller ..

---
+++ CoreDemo/Controllers/MessageController.cs
@@ -0,0 +1,20 @@
+﻿using BLL.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class MessageController : Controller
+    {
+        private readonly IMessage2Service _messageService;
+        public MessageController(IMessage2Service messageService)
+        {
+            this._messageService = messageService ?? throw new ArgumentNullException(nameof(messageService));
+        }
+        public async Task<IActionResult> InBox()
+        {
+            int id = 2;
+            var values = await _messageService.GetInBoxListByWriter(id);
+            return View();
+        }
+    }
+}
