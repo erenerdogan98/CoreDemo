25d202e2124130ed20673439e7aa92d16f777f09
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-27 13:22:52 +0300

    added special method ..

--- DAL/Abstract/IMessage2DAL.cs
+++ DAL/Abstract/IMessage2DAL.cs
@@ -4,5 +4,6 @@ namespace DAL.Abstract
 {
     public interface IMessage2DAL : IGenericDAL<Message2>
     {
+        Task<List<Message2>> GetListWithMessageByWriterAsync(int id);
     }
 }
--- DAL/EntityFramework/EFMessage2Repository.cs
+++ DAL/EntityFramework/EFMessage2Repository.cs
@@ -2,14 +2,27 @@
 using DAL.Context;
 using DAL.Repositories;
 using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace DAL.EntityFramework
 {
     public class EFMessage2Repository : GenericRepository<Message2> , IMessage2DAL
     {
+        private readonly MyContext _context;
         public EFMessage2Repository(MyContext context) : base(context)
         {
-            
+            this._context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<List<Message2>> GetListWithMessageByWriterAsync(int id) => await CustomQueryWithIncludes(x => x.ReceiverUser)
+            .Where(x => x.ReceiverID == id)
+            .ToListAsync();
+
+        private IQueryable<Message2> CustomQueryWithIncludes(params Expression<Func<Message2, object>>[] includeProperties)
+        {
+            IQueryable<Message2> query = _context.Set<Message2>();
+            return includeProperties.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
         }
     }
 }
