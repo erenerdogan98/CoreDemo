3135d82f8a989f0562d58c1f3dcbc86c13f27cfb
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-27 13:07:42 +0300

    added Message2Manager class and implemented IMessage2Service ..

---
+++ BLL/Concrete/Message2Manager.cs
@@ -0,0 +1,29 @@
+﻿using BLL.Abstract;
+using DAL.Abstract;
+using Entities.Concrete;
+using System.Linq.Expressions;
+
+namespace BLL.Concrete
+{
+    public class Message2Manager : IMessage2Service
+    {
+        private readonly IMessage2DAL _message2DAL;
+        public Message2Manager(IMessage2DAL message2DAL)
+        {
+            this._message2DAL = message2DAL ?? throw new ArgumentNullException(nameof(message2DAL));
+        }
+        public async Task AddAsync(Message2 message) => await _message2DAL.AddAsync(message);
+
+        public async Task DeleteAsync(Message2 message) => await _message2DAL.DeleteAsync(message);
+
+        public async Task<IEnumerable<Message2>> GetAllAsync() => await _message2DAL.GetAllAsync();
+
+        public async Task<IEnumerable<Message2>> GetAllAsync(Expression<Func<Message2, bool>> filter) => await _message2DAL.GetAllAsync(filter);
+
+        public async Task<Message2> GetByIdAsync(int id) => await _message2DAL.GetByIdAsync(id);
+
+        public async Task<IEnumerable<Message2>> GetInBoxListByWriter(int receiverId) => await _message2DAL.GetAllAsync(x => x.ReceiverID == receiverId);
+
+        public async Task UpdateAsync(Message2 message) => await _message2DAL.UpdateAsync(message);
+    }
+}
