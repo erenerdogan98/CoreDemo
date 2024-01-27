8d07fbb2096aa5f19f72f295b9dc704b464221cb
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-27 12:57:09 +0300

    added Entity framework repository class  for message 2

---
+++ DAL/EntityFramework/EFMessage2Repository.cs
@@ -0,0 +1,15 @@
+﻿using DAL.Abstract;
+using DAL.Context;
+using DAL.Repositories;
+using Entities.Concrete;
+
+namespace DAL.EntityFramework
+{
+    public class EFMessage2Repository : GenericRepository<Message2> , IMessage2DAL
+    {
+        public EFMessage2Repository(MyContext context) : base(context)
+        {
+            
+        }
+    }
+}
