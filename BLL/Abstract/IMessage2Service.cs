b14394d05541b3d801bf0e37810269c976a6afd6
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-27 13:07:18 +0300

    added IMessage2 service ..

---
+++ BLL/Abstract/IMessage2Service.cs
@@ -0,0 +1,9 @@
+﻿using Entities.Concrete;
+
+namespace BLL.Abstract
+{
+    public interface IMessage2Service : IGenericService<Message2>
+    {
+        Task<IEnumerable<Message2>> GetInBoxListByWriter(int receiverId);
+    }
+}
