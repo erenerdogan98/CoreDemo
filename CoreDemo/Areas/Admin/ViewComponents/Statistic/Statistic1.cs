c0e1cbf8b6b95c8ccbeb2408e7d66cde0532e400
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-28 15:39:16 +0300

    added ViewBags ...

--- CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BLL.Abstract;
+using DAL.Context;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
 {
     public class Statistic1 : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IBlogService _blogService;
+        private readonly MyContext _context;
+        public Statistic1(IBlogService blogService, MyContext context)
         {
+            _blogService = blogService ?? throw new ArgumentNullException(nameof(blogService));
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        public async Task<IViewComponentResult> Invoke()
+        {
+            var blogs = await _blogService.GetAllAsync();
+            var blogList = blogs.ToList();
+            ViewBag.v1 = blogList.Count();
+
+            ViewBag.v2 = _context.Contacts.Count();
+
+            ViewBag.v3 = _context.Comments.Count();
             return View();
         }
     }
