b2fb91799c5fedcdc57544ecd8d7874a0a7a0a77
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-28 14:15:15 +0300

    added ExportStaticExcelBlogList method ..

--- CoreDemo/Areas/Admin/Controllers/BlogController.cs
+++ CoreDemo/Areas/Admin/Controllers/BlogController.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ClosedXML.Excel;
+using CoreDemo.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Areas.Admin.Controllers
 {
     public class BlogController : Controller
     {
-        public IActionResult Index()
+        public IActionResult ExportStaticExcelBlogList()
         {
-            return View();
+            using (var workbook = new XLWorkbook())
+            {
+                var workSheet = workbook.Worksheets.Add("Blog List");
+                workSheet.Cell(1, 1).Value = "Blog ID"; // first row first column .. 
+                workSheet.Cell(1, 2).Value = "Blog Name"; // first row second column ..
+
+                int blogRowCount = 2; // i write 2 because first row we will have titles (Id and name) 
+                foreach (var item in GetBlogList())
+                {
+                    workSheet.Cell(blogRowCount, 1).Value = item.ID; // blorRowCount is row , 1 is column ..
+                    workSheet.Cell(blogRowCount, 2).Value = item.BlogName;
+                    blogRowCount++;
+                }
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application / vdn.openxmlformats - officedocument.spreadsheetml.sheet", "Worksheet1.xlsx");
+                }
+            }
         }
+        public List<BlogModel> GetBlogList()
+        {
+            List<BlogModel> blogModels = new List<BlogModel>
+            {
+                new BlogModel {ID=1, BlogName = "Introduction to C# programming"},
+                new BlogModel {ID=2, BlogName = "Introduction to SQL"},
+                new BlogModel {ID=3, BlogName = "Introduction to React.JS"}
+            };
+            return blogModels;
+        }
+
     }
 }
