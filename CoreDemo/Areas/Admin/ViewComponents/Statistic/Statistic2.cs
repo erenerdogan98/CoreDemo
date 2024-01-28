343b2913cc640a763d96ec51e6fceda9bf70d37d
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-28 16:42:06 +0300

    added ViewComponent

---
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic2.cs
@@ -0,0 +1,22 @@
+﻿using BLL.Abstract;
+using DAL.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic2 : ViewComponent
+    {
+        private readonly MyContext _context;
+        public Statistic2( MyContext context )
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        public async Task<IViewComponentResult> Invoke()
+        {
+            ViewBag.v2 = _context.Contacts.Count();
+
+            ViewBag.v3 = _context.Comments.Count();
+            return View();
+        }
+    }
+}
