765cd59a98ac68391fb352739e660a6f6c2f1dfc
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-28 17:05:20 +0300

    added Entity framework admin repository class ..

---
+++ DAL/EntityFramework/EFAdminRepository.cs
@@ -0,0 +1,15 @@
+﻿using DAL.Abstract;
+using DAL.Context;
+using DAL.Repositories;
+using Entities.Concrete;
+
+namespace DAL.EntityFramework
+{
+    public class EFAdminRepository : GenericRepository<Admin>, IAdminDAL
+    {
+        public EFAdminRepository(MyContext context) : base(context)
+        {
+            
+        }
+    }
+}
