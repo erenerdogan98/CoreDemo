4a277f4bb2d7404102dbee6f27897c0fe52f1c68
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-29 00:01:05 +0300

    added Chart controller and category list and returned Json ..

---
+++ CoreDemo/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,33 @@
+﻿using CoreDemo.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClass> categories = new List<CategoryClass>();
+            categories.Add(new CategoryClass
+            {
+                Name = "Technology",
+                Count = 10,
+            });
+            categories.Add(new CategoryClass
+            {
+                Name = "Software",
+                Count = 15,
+            });
+            categories.Add(new CategoryClass
+            {
+                Name = "Sport",
+                Count = 5,
+            });
+            return Json(new { jsonlist = categories});
+        }
+    }
+}
