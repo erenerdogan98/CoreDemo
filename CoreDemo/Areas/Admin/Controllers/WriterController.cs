ef5141a90af38e19d6e1b8eda9c3c2aa8b84b413
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-29 16:11:32 +0300

    added GetWriterById method and return Json. .

--- CoreDemo/Areas/Admin/Controllers/WriterController.cs
+++ CoreDemo/Areas/Admin/Controllers/WriterController.cs
@@ -19,6 +19,13 @@ namespace CoreDemo.Areas.Admin.Controllers
             return Json(jsonWriters);
         }
 
+        public IActionResult GetWriterById(int id)
+        {
+            var findWriter = writers.FirstOrDefault( x=> x.Id == id);
+            var jsonWriters = JsonConvert.SerializeObject(findWriter);
+            return Json(jsonWriters);
+        }
+
         public static List<WriterClass> writers = new List<WriterClass>
         {
             new WriterClass
