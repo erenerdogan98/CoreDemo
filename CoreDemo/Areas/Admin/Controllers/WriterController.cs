abfc31bf0d6cbc0d9c2d366ff175f8b9ee831249
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-29 15:33:03 +0300

    added Area and static List method for Writers..

--- CoreDemo/Areas/Admin/Controllers/WriterController.cs
+++ CoreDemo/Areas/Admin/Controllers/WriterController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoreDemo.Areas.Admin.Models;
+using DocumentFormat.OpenXml.Office2010.Excel;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class WriterController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
+
+        public static List<WriterClass> writers = new List<WriterClass>
+        {
+            new WriterClass
+            {
+                Id = 1,
+                Name = "Ahmet"
+            },
+            new WriterClass
+            {
+                Id = 2,
+                Name = "Ayşe"
+            },
+            new WriterClass
+            {
+                Id = 3,
+                Name = "Mehmet"
+            }
+        };
     }
 }
