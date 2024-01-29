036114bd935a1af134839e5fbafac7aee7d3e67c
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-29 15:30:31 +0300

    created writer controller

---
+++ CoreDemo/Areas/Admin/Controllers/WriterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
