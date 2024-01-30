0ad1d417f0e40f199bc53f6b8eb3396bb7986b2a
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-30 16:52:28 +0300

    added AddEmployee method

--- CoreDemo/Controllers/EmployeeTestController.cs
+++ CoreDemo/Controllers/EmployeeTestController.cs
@@ -1,17 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace CoreDemo.Controllers
 {
     public class EmployeeTestController : Controller
     {
+        private readonly HttpClient _httpClient;
+        public EmployeeTestController(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));    
+        }
         public async Task<IActionResult> Index()
         {
-            var httpClient = new HttpClient();
-            var responseMessage = await httpClient.GetAsync("https://localhost:44368/api/Default"); // from BlogCoreApıDemo , Default controoler GET/api/default..
+            var responseMessage = await _httpClient.GetAsync("https://localhost:44368/api/Default"); // from BlogCoreApıDemo , Default controoler GET/api/default..
             var jsonString = await responseMessage.Content.ReadAsStringAsync();
             var values = JsonConvert.DeserializeObject<List<Class1>>(jsonString);
             return View();
+        }
+
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+        public async Task<IActionResult> AddEmployee(Class1 employee) 
+        {
+            var jsonEmployee = JsonConvert.SerializeObject(employee);
+            StringContent stringContent = new StringContent(jsonEmployee,Encoding.UTF8,"application/json");
+            var responseMessage = await _httpClient.PostAsync("https://localhost:44368/api/Default", stringContent);
+            if (responseMessage.IsSuccessStatusCode) 
+            {
+                return RedirectToAction("Index");
+            }
+            return View(employee);
         }
     }
     public class Class1
