eca1ba10fc49f3932af0c978852f2fc46f486daf
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-31 00:55:13 +0300

    added AppUser entity , inherited IdentityUser

---
+++ Entities/Concrete/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Entities.Concrete
+{
+    public class AppUser : IdentityUser
+    {
+        public string NameSurname { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
