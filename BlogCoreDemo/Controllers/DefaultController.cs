c9e7727fccd3bd5e0752fd84521ccf1d189050df
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-30 16:15:48 +0300

    added EmployeeList method

---
+++ BlogCoreDemo/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using BlogCoreDemo.DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCoreAPIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        private readonly Context _context;
+        public DefaultController(Context context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        [HttpGet]
+        public IActionResult EmployeeList()
+        {
+            var employees = _context.Employees.ToList();
+            return Ok(employees);
+        }
+    }
+}
