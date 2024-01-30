946b871f54530b86e3e507a862634a00341f4650
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-30 16:30:25 +0300

    added EmplyeUpdate method ..

--- BlogCoreDemo/Controllers/DefaultController.cs
+++ BlogCoreDemo/Controllers/DefaultController.cs
@@ -52,5 +52,23 @@ namespace BlogCoreAPIDemo.Controllers
                 return Ok();
             }
         }
+
+        [HttpPut("{id}")]
+        public IActionResult EmployeeUpdate(Employee updatedEmployee)
+        {
+            var id = updatedEmployee.ID;
+            var emp = _context.Employees.Find(id);
+            if (emp == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                emp.Name = updatedEmployee.Name;
+                _context.Employees.Update(updatedEmployee);
+                _context.SaveChanges();
+                return Ok();
+            }
+        }
     }
 }
