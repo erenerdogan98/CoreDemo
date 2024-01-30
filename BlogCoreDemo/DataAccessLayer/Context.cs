9f21bb4371085a0cd7c6858568b068f398751e6a
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-30 14:52:15 +0300

    added OnConfiguring for Sql connection , and DbSet for Employee

--- BlogCoreDemo/DataAccessLayer/Context.cs
+++ BlogCoreDemo/DataAccessLayer/Context.cs
@@ -1,6 +1,13 @@
-﻿namespace BlogCoreDemo.DataAccessLayer
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogCoreDemo.DataAccessLayer
 {
-    public class Context
+    public class Context : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-57R498V\\SQLEXPRESS01;database=CoreBlogApiDb; integrated security=true; TrustServerCertificate=true;");
+        }
+        public DbSet<Employee> Employees { get; set; }
     }
 }
