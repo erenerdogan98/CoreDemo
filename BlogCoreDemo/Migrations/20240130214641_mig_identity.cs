66b731b172e7393ecbbda2fd9f36ad77b076906c
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-31 00:51:33 +0300

    ..

---
+++ BlogCoreDemo/Migrations/20240130214641_mig_identity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogCoreAPIDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_identity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
