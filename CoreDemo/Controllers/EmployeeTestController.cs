d664fbd250a87f3179dc5ee526a531efa035d7a0
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-30 18:22:01 +0300

    added EditEmployee metho (HttpPost)

--- CoreDemo/Controllers/EmployeeTestController.cs
+++ CoreDemo/Controllers/EmployeeTestController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DocumentFormat.OpenXml.Office2010.Excel;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Text;
 
@@ -9,7 +10,7 @@ namespace CoreDemo.Controllers
         private readonly HttpClient _httpClient;
         public EmployeeTestController(HttpClient httpClient)
         {
-            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));    
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
         }
         public async Task<IActionResult> Index()
         {
@@ -24,12 +25,12 @@ namespace CoreDemo.Controllers
             return View();
         }
         [HttpPost]
-        public async Task<IActionResult> AddEmployee(Class1 employee) 
+        public async Task<IActionResult> AddEmployee(Class1 employee)
         {
             var jsonEmployee = JsonConvert.SerializeObject(employee);
-            StringContent stringContent = new StringContent(jsonEmployee,Encoding.UTF8,"application/json");
+            StringContent stringContent = new StringContent(jsonEmployee, Encoding.UTF8, "application/json");
             var responseMessage = await _httpClient.PostAsync("https://localhost:44368/api/Default", stringContent);
-            if (responseMessage.IsSuccessStatusCode) 
+            if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index");
             }
@@ -39,13 +40,25 @@ namespace CoreDemo.Controllers
         public async Task<IActionResult> EditEmployee(int id)
         {
             var responseMessage = await _httpClient.GetAsync($"https://localhost:44368/api/Default/{id}");
-            if (responseMessage.IsSuccessStatusCode) 
+            if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonEmployee = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<Class1>(jsonEmployee);
                 return View();
             }
             return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditEmployee(Class1 employee)
+        {
+            var jsonEmployee = JsonConvert.SerializeObject(employee);
+            var content = new StringContent(jsonEmployee, Encoding.UTF8, "application/json");
+            var responseMessage = await _httpClient.PutAsync("https://localhost:44368/api/Default", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(employee);
         }
     }
     public class Class1
