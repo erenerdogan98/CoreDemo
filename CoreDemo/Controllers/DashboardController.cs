428acaf398636ca1e0a9feeb88b26c2a52126bf7
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-31 23:50:14 +0300

    added some values and changed writer id 1 to writerId ...

--- CoreDemo/Controllers/DashboardController.cs
+++ CoreDemo/Controllers/DashboardController.cs
@@ -6,12 +6,21 @@ namespace CoreDemo.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly MyContext _context;
+        public DashboardController(MyContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
         public IActionResult Index()
         {
-            MyContext c = new MyContext();
-            ViewBag.v1 = c.Blogs.Count().ToString();
-            ViewBag.v2 = c.Blogs.Where(x => x.WriterID == 1).Count().ToString();
-            ViewBag.v3 = c.Categories.Count().ToString();
+
+            var userName = User.Identity.Name;
+            var userMail = _context.Users.Where(x =>  x.UserName == userName).Select(y => y.Email).FirstOrDefault();
+            var writerId = _context.Writers.Where(x => x.Email == userName).Select(y => y.ID).FirstOrDefault();
+
+            ViewBag.v1 = _context.Blogs.Count().ToString();
+            ViewBag.v2 = _context.Blogs.Where(x => x.WriterID == writerId).Count().ToString();
+            ViewBag.v3 = _context.Categories.Count().ToString();
             return View();
         }
     }
