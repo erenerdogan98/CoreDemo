68102731ebfea9b2ed1c1b8bfea6266448d5e485
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-31 17:45:47 +0300

    added for sign in ..

---
+++ CoreDemo/Models/UserSignInViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreDemo.Models
+{
+    public class UserSignInViewModel
+    {
+        [Required(ErrorMessage ="Please , write your User Name!")]
+        public string userName { get; set; }
+        [Required(ErrorMessage = "Please , write your password")]
+        public string password { get; set; }
+    }
+}
