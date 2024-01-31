f4dea4d520947f49e80cb6a267de43025a0e35d1
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-31 10:00:52 +0300

    added HttpGet and HttpPost (with codes to add user) attributes ..

--- CoreDemo/Controllers/RegisterUserController.cs
+++ CoreDemo/Controllers/RegisterUserController.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using CoreDemo.Models;
+using Entities.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace CoreDemo.Controllers
 {
@@ -11,9 +13,37 @@ namespace CoreDemo.Controllers
         {
             _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
         }
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserSignUpViewModel p)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser()
+                {
+                    Email = p.Mail,
+                    UserName = p.UserName,
+                    NameSurname = p.UserName
+                };
+                var result = await _userManager.CreateAsync(appUser,p.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index","Login");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View(p);
         }
 
     }
