99fd1513cd4dd599c2b9b4959894c70ffe7f0f98
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-31 09:53:57 +0300

    added view model calss for sign up ..

---
+++ CoreDemo/Models/UserSignUpViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreDemo.Models
+{
+    public class UserSignUpViewModel
+    {
+        [Display(Name = "Name Surname")]
+        [Required(ErrorMessage = "Please , write your name and surname.")]
+        public string nameSurname { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Please , write password.")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [Required(ErrorMessage = "Passwords do not match!")]
+        public string ConfirmPassword { get; set; }
+
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "Please , write your email address.")]
+        public string Mail { get; set; }
+
+        [Display(Name = "User Name")]
+        [Required(ErrorMessage = "Please, write your user name.")]
+        public string UserName { get; set; }
+    }
+}
