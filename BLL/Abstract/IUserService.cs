21eb40a27fdc7ce9312aa2f678dd1f32c52e0e59
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-02-01 00:24:18 +0300

    added User service .. will refactore ..

---
+++ BLL/Abstract/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace BLL.Abstract
+{
+    public interface IUserService<T>
+    {
+        Task AddAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> filter);
+    }
+}
