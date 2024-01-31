bc79d76552908e28796058e02cfa169ca80ada1f
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-01-31 09:56:13 +0300

    added UserManager for AppUser and defined in constructor ..

--- CoreDemo/Controllers/RegisterUserController.cs
+++ CoreDemo/Controllers/RegisterUserController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
     public class RegisterUserController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+        public RegisterUserController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+        }
         public IActionResult Index()
         {
             return View();
         }
+
     }
 }
