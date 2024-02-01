c90683409fc262937af21b6a0a08a51c163a1435
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-02-01 15:51:18 +0300

    changed InBox method to use Identity ..

--- CoreDemo/Controllers/MessageController.cs
+++ CoreDemo/Controllers/MessageController.cs
@@ -1,4 +1,5 @@
 ﻿using BLL.Abstract;
+using DAL.Context;
 using Entities.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -10,15 +11,19 @@ namespace CoreDemo.Controllers
     public class MessageController : Controller
     {
         private readonly IMessage2Service _messageService;
-        public MessageController(IMessage2Service messageService)
+        private readonly MyContext _context;
+        public MessageController(IMessage2Service messageService, MyContext context)
         {
-            this._messageService = messageService ?? throw new ArgumentNullException(nameof(messageService));
+            _messageService = messageService ?? throw new ArgumentNullException(nameof(messageService));
+            _context = context ?? throw new ArgumentNullException("context");
         }
         public async Task<IActionResult> InBox()
         {
-            int id = 2;
-            var values = await _messageService.GetInBoxListByWriter(id);
-            return View();
+            var username = User.Identity.Name;
+            var usermail = _context.Users.Where(x => x.NameSurname == username).Select(y => y.Email).FirstOrDefault();
+            var writerId = _context.Writers.Where(x => x.Email == usermail).Select(y => y.ID).FirstOrDefault();
+            var values = await _messageService.GetInBoxListByWriter(writerId);
+            return View(values);
         }
         public async Task<IActionResult> MessageDetails(int id)
         {
