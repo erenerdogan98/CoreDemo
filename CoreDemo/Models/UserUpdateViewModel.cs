b0460cb5b089e45ed786fa292ced4d03327682fc
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-02-01 15:25:18 +0300

    added Username prop ..

--- CoreDemo/Models/UserUpdateViewModel.cs
+++ CoreDemo/Models/UserUpdateViewModel.cs
@@ -3,6 +3,7 @@
     public class UserUpdateViewModel
     {
         public string namesurname { get; set; }
+        public string username { get; set; }
         public string mail { get; set; }
         public string imageurl { get; set; }
     }
