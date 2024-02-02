3a84de4a078d76c6f62d908c9e60c9dbc87decad
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-02-02 17:13:58 +0300

    added values ..

--- CoreDemo/Areas/Admin/Controllers/AdminBlogController.cs
+++ CoreDemo/Areas/Admin/Controllers/AdminBlogController.cs
@@ -1,13 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BLL.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class AdminBlogController : Controller
     {
-        public IActionResult Index()
+        private readonly IBlogService _blogService;
+        public AdminBlogController(IBlogService blogService)
         {
-            return View();
+            _blogService = blogService ?? throw new ArgumentNullException(nameof(blogService));
+        }
+        public async Task<IActionResult> Index()
+        {
+            var values = await _blogService.GetListWithCategoryAsync();
+            return View(values);
         }
     }
 }
