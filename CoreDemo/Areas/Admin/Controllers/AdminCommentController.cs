201d2dd755d9955a3332de9c0a812fb77e7a55f6
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-02-02 18:14:09 +0300

    added values , to List with blog

--- CoreDemo/Areas/Admin/Controllers/AdminCommentController.cs
+++ CoreDemo/Areas/Admin/Controllers/AdminCommentController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BLL.Abstract;
+using DocumentFormat.OpenXml.ExtendedProperties;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class AdminCommentController : Controller
     {
+        private readonly ICommentService _commentService;
+        public AdminCommentController(ICommentService commentService)
+        {
+            _commentService = commentService ?? throw new ArgumentNullException(nameof(commentService));
+        }
         public IActionResult Index()
         {
+            var values = _commentService.GetListWithBlogAsync();
             return View();
         }
     }
