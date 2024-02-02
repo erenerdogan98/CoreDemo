0da0fe60ef18ad2a569f6e0795c64755a1d5626a
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-02-02 14:44:12 +0300

    added İnbox methods to use Identity ..

--- CoreDemo/Areas/Admin/Controllers/AdminMessageController.cs
+++ CoreDemo/Areas/Admin/Controllers/AdminMessageController.cs
@@ -1,13 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BLL.Abstract;
+using DAL.Context;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class AdminMessageController : Controller
     {
-        public IActionResult Inbox()
+        private readonly MyContext _context;
+        private readonly IMessage2Service _messageService;
+        public AdminMessageController(MyContext context, IMessage2Service messageService)
         {
-            return View();
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _messageService = messageService ?? throw new ArgumentNullException(nameof(messageService));
+        }
+        public async Task<IActionResult> Inbox()
+        {
+            var username = User.Identity.Name;
+            var usermail = _context.Users.Where(x => x.NameSurname == username).Select(y => y.Email).FirstOrDefault();
+            var writerId = _context.Writers.Where(x => x.Email == usermail).Select(y => y.ID).FirstOrDefault();
+            var values = await _messageService.GetInBoxListByWriter(writerId);
+            return View(values);
         }
     }
 }
