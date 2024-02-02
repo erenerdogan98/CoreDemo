a19f1a71d3098447aa9a44f8933739830942f1ed
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-02-02 20:03:30 +0300

    added AddRole methods..

--- CoreDemo/Areas/Admin/Controllers/AdminRoleController.cs
+++ CoreDemo/Areas/Admin/Controllers/AdminRoleController.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using CoreDemo.Models;
+using Entities.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,6 +17,33 @@ namespace CoreDemo.Areas.Admin.Controllers
         {
             var values = _roleManager.Roles.ToList();
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddRole(RoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppRole role = new AppRole
+                {
+                    Name = model.Name
+                };
+
+                var result = await _roleManager.CreateAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+            }
+            return View(model);
         }
     }
 }
