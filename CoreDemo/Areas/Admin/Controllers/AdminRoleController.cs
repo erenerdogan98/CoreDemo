729c8a01a936a4fb30a525c66c5a5fc0c59b36f7
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-02-02 19:50:56 +0300

    addedd values parameter , Roles. to list ..

--- CoreDemo/Areas/Admin/Controllers/AdminRoleController.cs
+++ CoreDemo/Areas/Admin/Controllers/AdminRoleController.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class AdminRoleController : Controller
     {
+        private readonly RoleManager<AppRole> _roleManager;
+        public AdminRoleController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager ?? throw new ArgumentNullException(nameof(roleManager));
+        }
         public IActionResult Index()
         {
-            return View();
+            var values = _roleManager.Roles.ToList();
+            return View(values);
         }
     }
 }
