89b2fff78dc26f27e6a0327bfaf1d7aa17439402
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-02-07 15:46:00 +0300

    changed async ..

--- CoreDemo/Controllers/RegisterController.cs
+++ CoreDemo/Controllers/RegisterController.cs
@@ -6,39 +6,39 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
-	public class RegisterController : Controller
-	{
-		private readonly IWriterService _writerService;
-		private readonly WriterValidator _writerValidator;
+    public class RegisterController : Controller
+    {
+        private readonly IWriterService _writerService;
+        private readonly WriterValidator _writerValidator;
         public RegisterController(IWriterService writerService, WriterValidator writerValidator)
         {
             _writerService = writerService;
-			_writerValidator = writerValidator;
+            _writerValidator = writerValidator;
         }
         [HttpGet]
-		public IActionResult Index()
-		{
-			return View();
-		}
-		[HttpPost]
-		public IActionResult Index(Writer writer)
-		{
-			ValidationResult validationResult = _writerValidator.Validate(writer);
-			if(validationResult.IsValid)
-			{
-				writer.Status = true;
-				writer.About = "Test";
-				_writerService.AddAsync(writer);
-				return RedirectToAction("Index", "Blog");
-			}
-			else
-			{
-				foreach(var item in validationResult.Errors)
-				{
-					ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
-				}
-			}
-			return View();
-		}
-	}
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(Writer writer)
+        {
+            ValidationResult validationResult = _writerValidator.Validate(writer);
+            if (validationResult.IsValid)
+            {
+                writer.Status = true;
+                writer.About = "Test";
+                await _writerService.AddAsync(writer);
+                return RedirectToAction("Index", "Blog");
+            }
+            else
+            {
+                foreach (var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
 }
