dcbe2ce183c1078e2d17e17143eea7651bc892be
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-02-07 17:38:08 +0300

    writed codes to take last 3 blogs ..

--- CoreDemo/ViewComponents/Blog/BlogLastThreePost.cs
+++ CoreDemo/ViewComponents/Blog/BlogLastThreePost.cs
@@ -5,15 +5,21 @@ namespace CoreDemo.ViewComponents.Blog
 {
     public class BlogLastThreePost : ViewComponent
     {
-        private readonly IBlogService _blogServce;
+        private readonly IBlogService _blogService;
         public BlogLastThreePost(IBlogService blogService)
         {
-            _blogServce = blogService;
+            _blogService = blogService;
         }
         public IViewComponentResult Invoke()
         {
-            // will write later
-            return View();
+            // To take last 3 blogs with LINQ
+            var lastThreeBlogs = _blogService.GetAllAsync().Result
+                .OrderBy(blog => blog.CreateDate) // To date (first one , the latest)
+                .TakeLast(3) // take last 3 
+                .ToList();
+
+            return View(lastThreeBlogs);
+ 
         }
     }
 }
